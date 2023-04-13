99a637cbebd51e396dccb4839d02d7afef86a8d6
Mariam Muchai <mariammuchai@dhcp-10-105-54-16.wireless.northwestern.private>
2023-04-13 11:03:13 -0500

    implemented log out functionality

--- react-firebase-app/src/context/AuthContext.js
+++ react-firebase-app/src/context/AuthContext.js
@@ -1,20 +1,24 @@
 import { createContext, useReducer } from "react";
 
-export const AuthContext = createContext();
+export const AuthContext = createContext(); 
 
 export const authReducer = (state, action) => {
     switch (action.type) {
+        case "LOGIN":
+            return { ...state, user: action.payload}
+        case "LOGOUT":
+            return { ...state, user: null}
 
         default:
             return state;
-    }
+    }  
 }
 
 export const AuthContextProvider = ({ children}) => {
     const [state, dispatch] = useReducer(authReducer, {
         user: null
     })
-
+    console.log('AuthContextProvider', state)
     return (
         <AuthContext.Provider value={{ ...state, dispatch }}>
             {children}
